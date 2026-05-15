b7a228f442b89f7f7b9b80dd8e928f0f1d5e22b9
Taras Ovdienko <ovdienkotaras@gmail.com>
2026-05-15 15:46:00 +0400

    добавлена модель User

---
+++ src/main/java/com/finsync/model/User.java
@@ -0,0 +1,34 @@
+  package com.finsync.model;
+
+  import org.springframework.data.annotation.Id;
+  import org.springframework.data.relational.core.mapping.Table;
+
+  @Table("users")
+  public class User {
+
+      @Id
+      private Integer id;
+      private String email;
+      private String password;
+      private String role;
+
+      public User() {}
+
+      public User(String email, String password, String role) {
+          this.email = email;
+          this.password = password;
+          this.role = role;
+      }
+
+      public Integer getId() { return id; }
+      public void setId(Integer id) { this.id = id; }
+
+      public String getEmail() { return email; }
+      public void setEmail(String email) { this.email = email; }
+
+      public String getPassword() { return password; }
+      public void setPassword(String password) { this.password = password; }
+
+      public String getRole() { return role; }
+      public void setRole(String role) { this.role = role; }
+  }
