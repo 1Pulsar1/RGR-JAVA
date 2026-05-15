db63d18d99feeeab6399899acf518f99529fcc79
Taras Ovdienko <ovdienkotaras@gmail.com>
2026-05-15 11:20:00 +0400

    добавлена точка входа и конфигурация приложения

---
+++ src/main/java/com/finsync/FinSyncApplication.java
@@ -0,0 +1,11 @@
+package com.finsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinSyncApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinSyncApplication.class, args);
+    }
+}
